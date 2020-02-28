d93bdffe53283043a64221150a4926ad41f7d092
etomeo <emanuele.tomeo@duedil.com>
2020-02-28 17:11:53 +0000

    Attempt to fix docker pull

--- src/buildscripts/cloudbuild-docker-build-push.sh
+++ src/buildscripts/cloudbuild-docker-build-push.sh
@@ -3,7 +3,7 @@
 BRANCH_NAME=$1
 SHORT_SHA=$2
 
-if [ $BRANCH_NAME = 'master' ]; then
+if [ $BRANCH_NAME = 'feat-fix-docker-pull' ]; then
       docker build . -f src/buildscripts/Dockerfile -t gcr.io/sandpit-environment/github.com/duedil-ltd/cicd-test:$SHORT_SHA
       docker push gcr.io/sandpit-environment/github.com/duedil-ltd/cicd-test:$SHORT_SHA
 else
