73d6e84a9a20caf9cf1ce8ee0be9a05b55d78b03
etomeo <emanuele.tomeo@duedil.com>
2020-03-06 14:05:07 +0000

    --

--- src/buildscripts/cloudbuild-spin-docker-build-push.sh
+++ src/buildscripts/cloudbuild-spin-docker-build-push.sh
@@ -4,7 +4,7 @@ BRANCH_NAME=$1
 SHORT_SHA=$2
 
 if [ $BRANCH_NAME = 'spinnaker' ]; then
-      docker build . -f src/buildscripts/Dockerfile -t eu.gcr.io/docker-registry/cicd-test-spin:$SHORT_SHA
+      docker build . -f src/buildscripts/Dockerfile -t eu.gcr.io/duedil-registry/cicd-test-spin:$SHORT_SHA
 else
       echo 'Not on spinnaker, skipping Docker step'
 fi
